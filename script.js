17285d90c853441d52904003d8192263e6a52b23
IsisHao3024N <izadoramariano323@gmail.com>
2026-08-10 23:19:40 -0300

    mudança no tamanho de 1024 para 1444

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-    const menuToggle = document.getElementById("menu-toggle");
-    const menuMobile = document.getElementById("menu-mobile");
+const menuToggle = document.getElementById("menu-toggle");
+const menuMobile = document.getElementById("menu-mobile");
 
-    menuToggle.addEventListener("click", () => {
+menuToggle.addEventListener("click", () => {
 
-        menuMobile.classList.toggle("active");
-        menuToggle.classList.toggle("active");
+    menuMobile.classList.toggle("active");
+    menuToggle.classList.toggle("active");
 
-    });
+});
+
+document.querySelector("#whatsapp").addEventListener("click", () => {
+    window.open("https://wa.me/5535999258133", "_blank");
+});
+
+document.querySelector("#linkedin").addEventListener("click", () => {
+    window.open("https://www.linkedin.com/in/izadora-vitoria-silva/", "_blank");
+});
+
+document.querySelector("#github").addEventListener("click", () => {
+    window.open("https://github.com/IsisHao3024N", "_blank");
+});
+
+document.querySelector("#email").addEventListener("click", () => {
+    window.open("https://mail.google.com/mail/u/0/#inbox", "_blank");
+});
