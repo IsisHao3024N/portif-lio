468594a049ddc3cc1ad0a0530f1ee65b3e27a74d
IsisHao3024N <izadoramariano323@gmail.com>
2026-08-11 16:00:49 -0300

    Alterações finais

--- script.js
+++ script.js
@@ -23,3 +23,23 @@ document.querySelector("#github").addEventListener("click", () => {
 document.querySelector("#email").addEventListener("click", () => {
     window.open("https://mail.google.com/mail/u/0/#inbox", "_blank");
 });
+
+document.querySelector("#portifolio").addEventListener("click", () => {
+    window.open("https://github.com/IsisHao3024N/portif-lio", "_blank");
+});
+
+document.querySelector("#sistem").addEventListener("click", () => {
+    window.open("https://isishao3024n.github.io/cadastro/", "_blank");
+});
+
+document.querySelector("#sistemCod").addEventListener("click", () => {
+    window.open("https://github.com/IsisHao3024N/cadastro", "_blank");
+});
+
+document.querySelector("#prototipo").addEventListener("click", () => {
+    window.open("https://www.figma.com/design/24LGrouOIqSdYCnQ8iQ9w1/pastelaria?node-id=0-1&t=U0f9gEDZu6202Hrj-1", "_blank");
+});
+
+document.querySelector("#prototipoCod").addEventListener("click", () => {
+    window.open("https://github.com/IsisHao3024N/pastearia-liart", "_blank");
+});
